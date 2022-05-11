22922a10ca9bc908a53ff3212e48c5ace8f10fcb
harshads <harshad.shanbhag@gmail.com>
2022-05-10 20:39:33 -0700

    Update streamlit_app.py
    Adding custom menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import pandas
 import streamlit
 
 streamlit.title('Diner')
@@ -8,4 +9,5 @@ streamlit.text('🐔 Hard-Boiled Free-Range Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
