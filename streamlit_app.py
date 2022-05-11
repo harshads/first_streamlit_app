83be9f21e43504219848d876926189de23d58460
harshads <harshad.shanbhag@gmail.com>
2022-05-10 20:34:58 -0700

    Update streamlit_app.py
    Adding menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('Test')
+streamlit.title('Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinash & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Eggs')
+
